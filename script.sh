90912de7c6517f478e36bc8e5a3a89d7dc379d7c
tomer dachbash <tomasds123456@gmail.com>
2025-10-20 22:32:04 +0300

    modify script.sh

--- script.sh
+++ script.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 
 function check_result() {
-        if [ $? -ne 0 ]; then
-                echo "[ERROR] $1 failed. Exiting."
-                exit 1
-        fi
-        echo "[SUCCESS] $1 succeeded."
+    if [ $? -ne 0 ]; then
+        echo "[ERROR] $1 failed. Exiting."
+        exit 1
+    fi
+    echo "[SUCCESS] $1 succeeded."
 }
-sudo su -
+
 apt update -y
-check_result "Updating repsitories"
+check_result "Updating repositories"
+
+apt install -y git python3-venv python3-pip
+check_result "Installing dependencies"
+
+rm -rf domain-monitor-system
 git clone https://github.com/Tomer-ui/domain-monitor-system.git
 check_result "Downloading git repository"
+
 cd domain-monitor-system/
-python3 -m venv _venv_
-source _venv_/bin/activate
+
+python3 -m venv venv
+source venv/bin/activate
 check_result "Creating virtual environment"
-pip3 install -r requirements.txt -y
+
+pip install -r requirements.txt
 check_result "Installing requirements"
-cp app.service /etc/system/systemd/app.service
-systemctl daemon-reload
-systemctl start app.service
-systemctl enable app.service
+
+sudo cp app.service /etc/systemd/system/app.service
+sudo systemctl daemon-reload
+sudo systemctl start app.service
+sudo systemctl enable app.service
 check_result "Starting service and enabling on boot startup"
